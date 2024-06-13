e7abf00cd6811a8988ea41cacbe7ae13745d703f
Abhigyan Deb <shadeslayer89.1463@gmail.com>
2024-06-13 23:10:42 +0530

    Day-2

--- Vite_01React/src/App.jsx
+++ Vite_01React/src/App.jsx
@@ -1,10 +1,15 @@
-
+import Demo from "./demo"
 
 function App() {
 
 
   return (
+                                                      // fragment(empty div tag) in react to prevent errors
+    <>                                       
     <h1>React Practice with Vite</h1>
+    <Demo/>
+    </>
+    
   )
 }
 
